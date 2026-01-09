433061a7b4130dbd38abdcb432ffea439f88868f
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2026-01-09 11:52:58 +0300

    adding  ModuleServiceCollectionExtensions
    Removed XML documentation comments for clarity.

--- src/Modules/ResearchProposal/ARAP.Modules.ResearchProposal.Api/ModuleServiceCollectionExtensions.cs
+++ src/Modules/ResearchProposal/ARAP.Modules.ResearchProposal.Api/ModuleServiceCollectionExtensions.cs
@@ -5,25 +5,18 @@ using ARAP.Modules.ResearchProposal.Infrastructure;
 
 namespace ARAP.Modules.ResearchProposal.Api;
 
-/// <summary>
-/// Extension methods for registering ResearchProposal module services
-/// </summary>
+
 public static class ModuleServiceCollectionExtensions
 {
-    /// <summary>
-    /// Registers all ResearchProposal module services (Application + Infrastructure layers)
-    /// </summary>
-    /// <param name="services">Service collection</param>
-    /// <param name="configuration">Application configuration</param>
-    /// <returns>Service collection for chaining</returns>
+    
     public static IServiceCollection AddResearchProposalModule(
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        // Register Application layer (MediatR handlers, validators)
+       
         services.AddResearchProposalApplication();
 
-        // Register Infrastructure layer (DbContext, repositories)
+       
         services.AddResearchProposalInfrastructure(configuration);
 
         return services;
