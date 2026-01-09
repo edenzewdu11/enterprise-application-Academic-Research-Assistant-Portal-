9cb304754cf208361d625c18250f05c51e6df73c
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2026-01-09 11:54:54 +0300

    adding SubmitProposalCommand
    Removed XML summary comments from SubmitProposalCommand.

--- src/Modules/ResearchProposal/ARAP.Modules.ResearchProposal.Application/Commands/SubmitProposal/SubmitProposalCommand.cs
+++ src/Modules/ResearchProposal/ARAP.Modules.ResearchProposal.Application/Commands/SubmitProposal/SubmitProposalCommand.cs
@@ -2,9 +2,7 @@ using MediatR;
 
 namespace ARAP.Modules.ResearchProposal.Application.Commands.SubmitProposal;
 
-/// <summary>
-/// Command to submit a proposal for review
-/// </summary>
+
 public sealed record SubmitProposalCommand : IRequest<Unit>
 {
     public Guid ProposalId { get; init; }
