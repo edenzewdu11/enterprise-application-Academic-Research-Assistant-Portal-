f0a72c0fbe6af3f9b97ff2771fb4177a60b68916
Hermi-git <andargiehermela1@gmail.com>
2026-01-09 22:50:59 +0300

    Add initial migration for Outbox schema and OutboxMessage table

---
+++ src/BuildingBlocks/ARAP.Infrastructure.Outbox/Migrations/20260108145315_InitialOutboxCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ARAP.Infrastructure.Outbox.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialOutboxCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "outbox");
+
+            migrationBuilder.CreateTable(
+                name: "outbox_messages",
+                schema: "outbox",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    OccurredOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ProcessedOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Error = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true),
+                    RetryCount = table.Column<int>(type: "integer", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_outbox_messages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_outbox_messages_occurred_on_utc",
+                schema: "outbox",
+                table: "outbox_messages",
+                column: "OccurredOnUtc");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_outbox_messages_processed_on_utc",
+                schema: "outbox",
+                table: "outbox_messages",
+                column: "ProcessedOnUtc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "outbox_messages",
+                schema: "outbox");
+        }
+    }
+}
