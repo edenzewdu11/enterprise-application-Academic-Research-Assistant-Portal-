b1206b62d707ea91366c1b2fa71b54700dad0e2c
Derartu Nigatu Tuge <167537446+derartuge19@users.noreply.github.com>
2026-01-09 13:36:45 +0300

    Fix XML comments in InitialCreate_Jan8 migration

--- src/Modules/DocumentReview/ARAP.Modules.DocumentReview.Infrastructure/Migrations/20260108103857_InitialCreate_Jan8.cs
+++ src/Modules/DocumentReview/ARAP.Modules.DocumentReview.Infrastructure/Migrations/20260108103857_InitialCreate_Jan8.cs
@@ -5,10 +5,10 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ARAP.Modules.DocumentReview.Infrastructure.Migrations
 {
-    /// <inheritdoc />
+   
     public partial class InitialCreate_Jan8 : Migration
     {
-        /// <inheritdoc />
+        
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.EnsureSchema(
