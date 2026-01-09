77dfb68e4674b65f9999e4b97116d5921b09b1a4
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2026-01-09 11:56:41 +0300

    adding DependencyInjection.cs
    Removed XML documentation comments from the DependencyInjection class.

--- src/Modules/ResearchProposal/ARAP.Modules.ResearchProposal.Application/DependencyInjection.cs
+++ src/Modules/ResearchProposal/ARAP.Modules.ResearchProposal.Application/DependencyInjection.cs
@@ -4,27 +4,21 @@ using System.Reflection;
 
 namespace ARAP.Modules.ResearchProposal.Application;
 
-/// <summary>
-/// Extension methods for registering ResearchProposal Application layer services
-/// </summary>
+
 public static class DependencyInjection
 {
-    /// <summary>
-    /// Registers MediatR handlers, validators, and other application services
-    /// </summary>
-    /// <param name="services">Service collection</param>
-    /// <returns>Service collection for chaining</returns>
+ 
     public static IServiceCollection AddResearchProposalApplication(this IServiceCollection services)
     {
         var assembly = Assembly.GetExecutingAssembly();
 
-        // Register MediatR for CQRS pattern
+        
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(assembly);
         });
 
-        // Register FluentValidation validators
+        
         services.AddValidatorsFromAssembly(assembly);
 
         return services;
