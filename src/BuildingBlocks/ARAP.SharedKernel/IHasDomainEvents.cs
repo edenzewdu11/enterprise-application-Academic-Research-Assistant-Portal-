912babd70a342bc0843afac18558745483a9d3c9
Derartu Nigatu Tuge <167537446+derartuge19@users.noreply.github.com>
2026-01-10 16:55:49 +0300

    docs: Add XML documentation to IHasDomainEvents interface

--- src/BuildingBlocks/ARAP.SharedKernel/IHasDomainEvents.cs
+++ src/BuildingBlocks/ARAP.SharedKernel/IHasDomainEvents.cs
@@ -1,10 +1,17 @@
 namespace ARAP.SharedKernel;
 
-
+/// <summary>
+/// Interface for entities that can raise domain events
+/// </summary>
 public interface IHasDomainEvents
 {
-    
+    /// <summary>
+    /// Gets all domain events raised by the entity
+    /// </summary>
     IReadOnlyList<IDomainEvent> GetDomainEvents();
 
-        void ClearDomainEvents();
+    /// <summary>
+    /// Clears all domain events from the entity
+    /// </summary>
+    void ClearDomainEvents();
 }
